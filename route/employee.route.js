4b24bb58f90411a5facb98ba24a1dff23dd0d641
Amanpal786 <amanpal.rpal@gmail.com>
2026-03-12 15:49:56 +0530

    update

--- route/employee.route.js
+++ route/employee.route.js
@@ -1,10 +1,93 @@
 import express from "express";
-import { getEmployees, addEmployee, deleteEmployee } from "../controller/employee.controller.js";
+import Employee from "../module/employee.model.js";
 
 const router = express.Router();
 
-router.get("/employees", getEmployees);
-router.post("/employees", addEmployee);
-router.delete("/employees/:id", deleteEmployee);
+
+// GET ALL EMPLOYEES
+router.get("/employees", async (req,res)=>{
+
+try{
+
+const employees = await Employee.find();
+
+res.json(employees);
+
+}catch(err){
+res.status(500).json(err);
+}
+
+});
+
+
+// GET SINGLE EMPLOYEE
+router.get("/employees/:id", async (req,res)=>{
+
+try{
+
+const employee = await Employee.findById(req.params.id);
+
+res.json(employee);
+
+}catch(err){
+res.status(500).json(err);
+}
+
+});
+
+
+// CREATE EMPLOYEE
+router.post("/employees", async (req,res)=>{
+
+try{
+
+const employee = new Employee(req.body);
+
+await employee.save();
+
+res.json(employee);
+
+}catch(err){
+res.status(500).json(err);
+}
+
+});
+
+
+// UPDATE EMPLOYEE
+router.put("/employees/:id", async (req,res)=>{
+
+try{
+
+const employee = await Employee.findByIdAndUpdate(
+req.params.id,
+req.body,
+{ new:true }
+);
+
+res.json(employee);
+
+}catch(err){
+res.status(500).json(err);
+}
+
+});
+
+
+// DELETE EMPLOYEE
+router.delete("/employees/:id", async (req,res)=>{
+
+try{
+
+await Employee.findByIdAndDelete(req.params.id);
+
+res.json({message:"Employee deleted"});
+
+}catch(err){
+res.status(500).json(err);
+}
+
+});
+
 
 export default router;
