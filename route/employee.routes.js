8c6fd4ddb6bdd0ffbcee0819b26c89a7474393d2
Amanpal786 <amanpal.rpal@gmail.com>
2026-03-12 15:45:02 +0530

    update

--- route/employee.routes.js
+++ route/employee.routes.js
@@ -1,22 +1,62 @@
 import express from "express";
+import Employee from "../module/employee.model.js";
+
 const router = express.Router();
 
-let employees = [];
 
-router.get("/employees", (req, res) => {
-  res.json(employees);
+// GET ALL EMPLOYEES
+router.get("/employees", async (req,res)=>{
+
+const employees = await Employee.find();
+
+res.json(employees);
+
 });
 
-router.post("/employees", (req, res) => {
-  const employee = req.body;
-  employees.push(employee);
-  res.json(employee);
+
+// GET SINGLE EMPLOYEE
+router.get("/employees/:id", async (req,res)=>{
+
+const employee = await Employee.findById(req.params.id);
+
+res.json(employee);
+
 });
 
-router.delete("/employees/:id", (req, res) => {
-  const id = req.params.id;
-  employees = employees.filter((e, i) => i != id);
-  res.json({ message: "deleted" });
+
+// CREATE EMPLOYEE
+router.post("/employees", async (req,res)=>{
+
+const employee = new Employee(req.body);
+
+await employee.save();
+
+res.json(employee);
+
+});
+
+
+// UPDATE EMPLOYEE
+router.put("/employees/:id", async (req,res)=>{
+
+const employee = await Employee.findByIdAndUpdate(
+req.params.id,
+req.body,
+{ new:true }
+);
+
+res.json(employee);
+
+});
+
+
+// DELETE EMPLOYEE
+router.delete("/employees/:id", async (req,res)=>{
+
+await Employee.findByIdAndDelete(req.params.id);
+
+res.json({message:"Employee deleted"});
+
 });
 
 export default router;
